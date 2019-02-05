79b637363733381b4579d2f0c200f7e1d6030a45
Brian Horowitz <guitarrook94@gmail.com>
2019-02-05 11:40:35 -0500

    Redid some of the seed data for images since they weren't rendering to the frontend based on url image extensions.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,38 +2,38 @@ User.create(username: "MoodPorker");
 User.create(username: 'Testy McTesterson');
 User.create(username: 'coffee_Dad');
 
-Contentimg.create(img_url: 'https://i.imgur.com/k2AhCJ2.gifv', user_id: 1)
+Contentimg.create(img_url: 'https://media.giphy.com/media/rBVM213ASKBIk/giphy.gif', user_id: 1)
 Contentimg.create(img_url: 'https://i.imgur.com/n77HHTT.gif', user_id: 1)
 Contentimg.create(img_url: 'https://i.imgur.com/u3fgjt1.gif', user_id: 1)
 Contentimg.create(img_url: 'https://i.imgur.com/flfjsk7.gif', user_id: 1)
 Contentimg.create(img_url: 'https://i.imgur.com/NJgCNHn.gif', user_id: 1)
 Contentimg.create(img_url: 'https://i.imgur.com/ul1NHA6.gif', user_id: 1)
 Contentimg.create(img_url: 'https://i.imgur.com/wZG9Rvh.gif', user_id: 1)
-Contentimg.create(img_url: 'https://i.imgur.com/OcEJT2P.gifv', user_id: 1)
-Contentimg.create(img_url: 'https://i.imgur.com/MxUqBSm.gifv', user_id: 1)
-Contentimg.create(img_url: 'https://preview.redd.it/bt86883kc8e21.png?width=768&auto=webp&s=caaf802888392bd74085bf392f69e13db0d3c095', user_id: 1)
+Contentimg.create(img_url: 'https://i.imgur.com/OcEJT2P.gif', user_id: 1)
+Contentimg.create(img_url: 'https://media.giphy.com/media/l2QDMVwaUjnxy0Ela/giphy.gif', user_id: 1)
+Contentimg.create(img_url: 'https://66.media.tumblr.com/4ff3e0594c35fc5a3ae8bb308511f33a/tumblr_mjf05bBj7A1s5nl47o1_500.gif', user_id: 1)
 
 Sadimg.create(img_url: 'https://i.imgur.com/BFBSOf5.gif', user_id: 1);
-Sadimg.create(img_url: 'https://i.imgur.com/uJ6ZQ8G.gifv', user_id: 1);
+Sadimg.create(img_url: 'https://media.giphy.com/media/glNFNf0ykGvm0/giphy.gif', user_id: 1);
 Sadimg.create(img_url: 'https://i.imgur.com/KaeqfdJ.gif', user_id: 1);
-Sadimg.create(img_url: 'https://i.imgur.com/IivSL1m.gifv', user_id: 1);
-Sadimg.create(img_url: 'https://i.imgur.com/HeQr39M.gifv', user_id: 1);
+Sadimg.create(img_url: 'https://media.giphy.com/media/LFR6TrnoOK6Fa/giphy.gif', user_id: 1);
+Sadimg.create(img_url: 'https://media.giphy.com/media/3ohhwBPhHjs9OBpIfC/giphy.gif', user_id: 1);
 Sadimg.create(img_url: 'https://i.redd.it/yqza20r65va21.gif', user_id: 1);
-Sadimg.create(img_url: 'https://i.imgur.com/xlq4INP.gifv', user_id: 1);
-Sadimg.create(img_url: 'https://i.imgur.com/0SvQtVH.gifv', user_id: 1);
-Sadimg.create(img_url: 'https://i.imgur.com/ZV5snZI.gifv', user_id: 1);
-Sadimg.create(img_url: 'https://i.imgur.com/NK7M2Ng.gifv', user_id: 1);
+Sadimg.create(img_url: 'https://media.giphy.com/media/3o7WIHLSsQnO0MkBBm/giphy.gif', user_id: 1);
+Sadimg.create(img_url: 'https://media.giphy.com/media/ukgcHmvflVWjwE8eU5/giphy.gif', user_id: 1);
+Sadimg.create(img_url: 'https://media.giphy.com/media/l2Sq3jYHktALKdQJO/giphy.gif', user_id: 1);
+Sadimg.create(img_url: 'https://media.giphy.com/media/l0Iymd27lJHsZVqjS/giphy.gif', user_id: 1);
 
 Happyimg.create(img_url: 'https://i.imgur.com/8uD1QIT.gif', user_id: 1);
-Happyimg.create(img_url: 'https://giphy.com/gifs/3orif4r259L1cu4Nhu/html5', user_id: 1);
-Happyimg.create(img_url: 'https://giphy.com/gifs/3o7btQxxrFH4VzTxvy/html5', user_id: 1);
-Happyimg.create(img_url: 'https://giphy.com/gifs/XuCwARWBIjrxe/html5', user_id: 1);
-Happyimg.create(img_url: 'https://giphy.com/gifs/xTiIzQh5iOuYiXHyXm/html5', user_id: 1);
-Happyimg.create(img_url: 'https://giphy.com/gifs/WFIkZlfyFLyCI/html5', user_id: 1);
-Happyimg.create(img_url: 'https://giphy.com/gifs/4Zo41lhzKt6iZ8xff9/html5', user_id: 1);
-Happyimg.create(img_url: 'https://giphy.com/gifs/C9x8gX02SnMIoAClXa/html5', user_id: 1);
-Happyimg.create(img_url: 'https://giphy.com/gifs/13MUyg7C3ilM8U/html5', user_id: 1);
-Happyimg.create(img_url: 'https://giphy.com/gifs/5ZkD28BJaaiME/html5', user_id: 1);
+Happyimg.create(img_url: 'https://media.giphy.com/media/3orif4r259L1cu4Nhu/giphy.gif', user_id: 1);
+Happyimg.create(img_url: 'https://media.giphy.com/media/3o7btQxxrFH4VzTxvy/giphy.gif', user_id: 1);
+Happyimg.create(img_url: 'https://media.giphy.com/media/XuCwARWBIjrxe/giphy.gif', user_id: 1);
+Happyimg.create(img_url: 'https://media.giphy.com/media/xTiIzQh5iOuYiXHyXm/giphy.gif', user_id: 1);
+Happyimg.create(img_url: 'https://media.giphy.com/media/WFIkZlfyFLyCI/giphy.gif', user_id: 1);
+Happyimg.create(img_url: 'https://media.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.gif', user_id: 1);
+Happyimg.create(img_url: 'https://media.giphy.com/media/C9x8gX02SnMIoAClXa/giphy.gif', user_id: 1);
+Happyimg.create(img_url: 'https://media.giphy.com/media/13MUyg7C3ilM8U/giphy.gif', user_id: 1);
+Happyimg.create(img_url: 'https://media.giphy.com/media/5ZkD28BJaaiME/giphy.gif', user_id: 1);
 
 Happynew.create(news_src: "https://www.goodnewsnetwork.org/dozens-of-real-life-jedis-are-finding-mental-relief-and-mindfulness-through-lightsaber-classes/", title: "Dozens of Real-Life Jedis Are Finding Mental Relief and Mindfulness Through Lightsaber Classes", description: "In a community center not so far away, Star Wars fans are battling it out like Jedi knights at a lightsaber combat academy.")
 Happynew.create(news_src: "https://www.goodnewsnetwork.org/mit-scientists-create-material-to-charge-our-phones-by-harvesting-wifi-from-air/", title: "Scientists Create Super-Thin ‘Sheet’ That Could Charge Our Phones By Harvesting Wifi From the Air", description: "Imagine a world where smartphones, laptops, wearables, and other electronics are powered without batteries. Researchers from MIT and elsewhere have taken a step in that direction, with the first fully flexible device that can convert energy from WiFi signals into electricity that could power our electronics.")
