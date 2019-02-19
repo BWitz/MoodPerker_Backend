8dcad933957cd141bfb7db0476cfe223c8bd3527
Brian Horowitz <guitarrook94@gmail.com>
2019-02-19 13:51:07 -0500

    Added final seeds and changed routes to only have the options available to the application, mainly index and create. Reiteration of this application could include more CRUD functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -2,29 +2,29 @@ Rails.application.routes.draw do
   namespace :api do
       namespace :v1 do
 
-        resources :users, only: [:index, :create, :update, :destroy]
+        resources :users, only: [:index]
 
-        resources :happyimgs, only: [:index, :create, :update, :destroy]
+        resources :happyimgs, only: [:index]
 
-        resources :sadimgs, only: [:index, :create, :update, :destroy]
+        resources :sadimgs, only: [:index]
 
-        resources :contentimgs, only: [:index, :create, :update, :destroy]
+        resources :contentimgs, only: [:index]
 
-        resources :boredimgs, only: [:index, :create, :update, :destroy]
+        resources :boredimgs, only: [:index]
 
-        resources :happynews, only: [:index, :create, :update, :destroy]
+        resources :happynews, only: [:index, :create]
 
-        resources :sadnews, only: [:index, :create, :update, :destroy]
+        resources :sadnews, only: [:index, :create]
 
-        resources :contentnews, only: [:index, :create, :update, :destroy]
+        resources :contentnews, only: [:index, :create]
 
-        resources :borednews, only: [:index, :create, :update, :destroy]
+        resources :borednews, only: [:index, :create]
 
-        resources :happyquotes, only: [:index, :create, :update, :destroy]
+        resources :happyquotes, only: [:index, :create]
 
-        resources :sadquotes, only: [:index, :create, :update, :destroy]
+        resources :sadquotes, only: [:index, :create]
 
-        resources :boredquotes, only: [:index, :create, :update, :destroy]
+        resources :boredquotes, only: [:index, :create]
       end
     end
 end
